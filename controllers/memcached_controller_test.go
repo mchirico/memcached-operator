a53b81cc39a1a6c927710fee570b2c964763f06e
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2021-03-07 17:02:35 -0500

    update-automated-save

--- controllers/memcached_controller_test.go
+++ controllers/memcached_controller_test.go
@@ -35,39 +35,46 @@ var _ = Describe("Namespecial controller", func() {
 				Namespace: namespace,
 			}
 
-			crd := &v1alpha1.Memcached{
+			cr := &v1alpha1.Memcached{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      key.Name,
 					Namespace: key.Namespace,
 				},
 			}
 
-			crd.Kind = kind
-			crd.Namespace = namespace
-			crd.Name = name
-			crd.Spec = v1alpha1.MemcachedSpec{}
-			crd.Spec.Size = 3
+			cr.Kind = kind
+			cr.Namespace = namespace
+			cr.Name = name
+			cr.Spec = v1alpha1.MemcachedSpec{}
+			cr.Spec.Size = 3
 
 			// We'll need to retry getting this, given that creation may not immediately happen.
 			Eventually(func() bool {
-				err := k8sClient.Create(ctx, crd)
+				err := k8sClient.Create(ctx, cr)
 				if err != nil {
 					return false
 				}
 				return true
 			}, timeout, interval).Should(BeTrue())
 
-			crd.Spec.Size = 2
+			cr.Spec.Size = 2
 			Eventually(func() bool {
-				err := k8sClient.Update(ctx, crd)
+				err := k8sClient.Update(ctx, cr)
 				if err != nil {
 					return false
 				}
 				return true
 			}, timeout, interval).Should(BeTrue())
 
+			//By("Expecting size to be 2")
+			//Eventually(func() int32 {
+			//	f := &v1alpha1.Memcached{}
+			//	_ = k8sClient.Get(context.Background(), key, f)
+			//	return f.Spec.Size
+			//}, timeout, interval).Should(Equal(2))
+
 			Eventually(func() bool {
-				err := k8sClient.Delete(ctx, crd)
+				err := k8sClient.Delete(ctx, cr)
 				if err != nil {
 					return false
 				}
